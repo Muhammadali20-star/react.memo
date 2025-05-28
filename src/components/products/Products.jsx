203861c9bdb4ef064e1b64b109edcb4fed30e6ab
Muhammadali <muhamadalimamadaminov@gmail.com>
2025-05-28 15:45:21 +0300

    user jsx

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -8,7 +8,7 @@ const Products = ({data}) => {
   <h2 className="text-center text-4xl font-semibold my-10 text-gray-800">Products</h2>
   <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
     {data?.map((item) => (
-      <div key={item.id}  className="bg-white p-4 rounded-xl shadow hover:shadow-lg transition-all duration-300 cursor-pointer" >
+      <div key={item.id}  className="bg-white p-4 rounded-xl shadow hover:shadow-lg" >
         <img src={item.image} onClick={() => navigate(`/product/${item.id}`)} alt={item.title} className="w-28 h-32 object-contain mx-auto mb-4" />
         <div className="space-y-1 text-sm text-gray-700">
           <h3 className="font-semibold truncate" title={item.title}>{item.title}</h3>
